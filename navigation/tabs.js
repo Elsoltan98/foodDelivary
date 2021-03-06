d1514a206c2d7eda33726070149b740e0e756035
Abdelhameed Mohamed <ben10a2992@gmail.com>
2021-03-06 18:07:35 +0200

    Add Header and icons in Home screen

--- navigation/tabs.js
+++ navigation/tabs.js
@@ -1,10 +1,85 @@
 import React from 'react';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {
+  BottomTabBar,
+  createBottomTabNavigator,
+} from '@react-navigation/bottom-tabs';
 import {Home} from './../screens';
-import {Image} from 'react-native';
+import {Image, TouchableOpacity, View} from 'react-native';
 import {COLORS, icons} from '../constants';
+import Svg, {Path} from 'react-native-svg';
+import {isIphoneX} from 'react-native-iphone-x-helper';
 
 const Tab = createBottomTabNavigator();
+
+const TabBarCustomButton = ({accessibilityState, children, onPress}) => {
+  const isSelected = accessibilityState.selected;
+
+  if (isSelected) {
+    return (
+      <View style={{flex: 1, alignItems: 'center'}}>
+        <View style={{flexDirection: 'row', position: 'absolute', top: 0}}>
+          <View style={{flex: 1, backgroundColor: COLORS.white}}></View>
+          <Svg width={75} height={61} viewBox="0 0 75 61">
+            <Path
+              d="M75.2 0v61H0V0c4.1 0 7.4 3.1 7.9 7.1C10 21.7 22.5 33 37.7 33c15.2 0 27.7-11.3 29.7-25.9.5-4 3.9-7.1 7.9-7.1h-.1z"
+              fill={COLORS.white}
+            />
+          </Svg>
+          <View style={{flex: 1, backgroundColor: COLORS.white}}></View>
+        </View>
+
+        <TouchableOpacity
+          style={{
+            top: -12.5,
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: 50,
+            height: 50,
+            borderRadius: 25,
+            backgroundColor: COLORS.white,
+          }}
+          onPress={onPress}>
+          {children}
+        </TouchableOpacity>
+      </View>
+    );
+  } else {
+    return (
+      <TouchableOpacity
+        style={{
+          flex: 1,
+          height: 60,
+          backgroundColor: COLORS.white,
+        }}
+        activeOpacity={1}
+        onPress={onPress}>
+        {children}
+      </TouchableOpacity>
+    );
+  }
+};
+
+const CustomTabBar = (props) => {
+  if (isIphoneX()) {
+    return (
+      <View>
+        <View
+          style={{
+            position: 'absolute',
+            left: 0,
+            bottom: 0,
+            right: 0,
+            height: 30,
+            backgroundColor: COLORS.white,
+          }}
+        />
+        <BottomTabBar {...props} />
+      </View>
+    );
+  } else {
+    return <BottomTabBar {...props} />;
+  }
+};
 
 const Tabs = () => (
   <Tab.Navigator
@@ -14,8 +89,10 @@ const Tabs = () => (
         backgroundColor: 'transparent',
         borderTopWidth: 0,
         elevation: 0,
+        marginBottom: 10,
       },
-    }}>
+    }}
+    tabBar={(props) => <CustomTabBar {...props} />}>
     <Tab.Screen
       name="Home"
       component={Home}
@@ -31,6 +108,7 @@ const Tabs = () => (
             }}
           />
         ),
+        tabBarButton: (props) => <TabBarCustomButton {...props} />,
       }}
     />
     <Tab.Screen
@@ -48,6 +126,7 @@ const Tabs = () => (
             }}
           />
         ),
+        tabBarButton: (props) => <TabBarCustomButton {...props} />,
       }}
     />
     <Tab.Screen
@@ -65,6 +144,7 @@ const Tabs = () => (
             }}
           />
         ),
+        tabBarButton: (props) => <TabBarCustomButton {...props} />,
       }}
     />
     <Tab.Screen
@@ -82,6 +162,7 @@ const Tabs = () => (
             }}
           />
         ),
+        tabBarButton: (props) => <TabBarCustomButton {...props} />,
       }}
     />
   </Tab.Navigator>
--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,79 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
+
+import {COLORS, FONTS, icons, SIZES} from '../constants';
 
 const Home = () => {
-  return (
-    <View>
-      <Text>Home Page</Text>
-    </View>
-  );
+  const renderHeader = () => {
+    return (
+      <View style={{flexDirection: 'row', height: 50}}>
+        <TouchableOpacity
+          style={{
+            width: 50,
+            paddingLeft: SIZES.padding * 2,
+            justifyContent: 'center',
+          }}>
+          <Image
+            source={icons.nearby}
+            resizeMode="contain"
+            style={{width: 30, height: 30}}
+          />
+        </TouchableOpacity>
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+          <View
+            style={{
+              width: '70%',
+              height: '100%',
+              backgroundColor: COLORS.lightGray3,
+              alignItems: 'center',
+              justifyContent: 'center',
+              borderRadius: SIZES.radius,
+            }}>
+            <Text style={{...FONTS.h3}}>Location</Text>
+          </View>
+        </View>
+        <TouchableOpacity
+          style={{
+            width: 50,
+            paddingRight: SIZES.padding * 2,
+            justifyContent: 'center',
+          }}>
+          <Image
+            source={icons.basket}
+            resizeMode="contain"
+            style={{width: 30, height: 30}}
+          />
+        </TouchableOpacity>
+      </View>
+    );
+  };
+
+  return <SafeAreaView style={styles.container}>{renderHeader()}</SafeAreaView>;
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.lightGray4,
+    marginTop: 10,
+  },
+  shadow: {
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 3,
+    elevation: 1,
+  },
+});
 
 export default Home;
