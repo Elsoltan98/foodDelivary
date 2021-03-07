1ecd9708875e96b83fa85a524357d56a9e069ee7
Abdelhameed Mohamed <ben10a2992@gmail.com>
2021-03-07 05:01:01 +0200

    Add list of categories in home screen

--- screens/Home.js
+++ screens/Home.js
@@ -6,6 +6,7 @@ import {
   SafeAreaView,
   TouchableOpacity,
   Image,
+  FlatList,
 } from 'react-native';
 
 import {COLORS, FONTS, icons, SIZES} from '../constants';
@@ -22,6 +23,15 @@ const Home = () => {
   const [currentLocation, setCurrentLocation] = React.useState(
     initialCurrentLocation,
   );
+
+  const onSelectedCategory = (category) => {
+    let resturantList = restaurantData.filter((a) =>
+      a.categories.includes(category.id),
+    );
+
+    setRestaurants(resturantList);
+    setSelectedCategory(category);
+  };
 
   const renderHeader = () => {
     return (
@@ -68,10 +78,64 @@ const Home = () => {
   };
 
   const renderMainCategories = () => {
+    const renderItem = ({item}) => {
+      return (
+        <TouchableOpacity
+          style={{
+            padding: SIZES.padding,
+            paddingBottom: SIZES.padding * 2,
+            backgroundColor:
+              selectedCategory?.id === item.id ? COLORS.primary : COLORS.white,
+            borderRadius: SIZES.radius,
+            alignItems: 'center',
+            justifyContent: 'center',
+            marginRight: SIZES.padding,
+            ...styles.shadow,
+          }}
+          onPress={() => onSelectedCategory(item)}>
+          <View
+            style={{
+              width: 50,
+              height: 50,
+              borderRadius: 25,
+              alignItems: 'center',
+              justifyContent: 'center',
+              backgroundColor:
+                selectedCategory?.id === item.id
+                  ? COLORS.white
+                  : COLORS.lightGray,
+            }}>
+            <Image
+              source={item.icon}
+              resizeMode="contain"
+              style={{width: 30, height: 30}}
+            />
+          </View>
+          <Text
+            style={{
+              marginTop: SIZES.padding,
+              color:
+                selectedCategory?.id === item.id ? COLORS.white : COLORS.black,
+              ...FONTS.body5,
+            }}>
+            {item.name}
+          </Text>
+        </TouchableOpacity>
+      );
+    };
+
     return (
       <View style={{padding: SIZES.padding * 2}}>
         <Text style={{...FONTS.h1}}>Main</Text>
         <Text style={{...FONTS.h1}}>Categories</Text>
+        <FlatList
+          horizontal
+          data={categories}
+          showsHorizontalScrollIndicator={false}
+          keyExtractor={(item) => `${item.id}`}
+          renderItem={renderItem}
+          contentContainerStyle={{paddingVertical: SIZES.padding * 2}}
+        />
       </View>
     );
   };
