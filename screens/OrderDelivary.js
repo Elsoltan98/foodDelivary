2cdc9d147a7d22ab1f4230e5225d8c0bdb92c676
Abdelhameed Mohamed <ben10a2992@gmail.com>
2021-03-12 23:47:04 +0200

    Add destinationMarker to orderDelivary screen

--- screens/OrderDelivary.js
+++ screens/OrderDelivary.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import {View, Text} from 'react-native';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import {View, Text, Image} from 'react-native';
+import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
+import {COLORS, icons} from '../constants';
 
 const OrederDelivary = ({route, navigation}) => {
   const [resturants, setResturants] = React.useState(null);
@@ -11,8 +12,6 @@ const OrederDelivary = ({route, navigation}) => {
 
   React.useEffect(() => {
     let {resturants, currentLocation} = route.params;
-
-    console.log(currentLocation);
 
     let fromLoc = currentLocation.gps;
     let toLoc = resturants.location;
@@ -32,12 +31,42 @@ const OrederDelivary = ({route, navigation}) => {
   }, [route.params]);
 
   const renderMap = () => {
+    const destinationMarker = () => (
+      <Marker coordinate={toLocation}>
+        <View
+          style={{
+            height: 40,
+            width: 40,
+            borderRadius: 20,
+            alignItems: 'center',
+            justifyContent: 'center',
+            backgroundColor: COLORS.white,
+          }}>
+          <View
+            style={{
+              height: 30,
+              width: 30,
+              borderRadius: 15,
+              alignItems: 'center',
+              justifyContent: 'center',
+              backgroundColor: COLORS.primary,
+            }}>
+            <Image
+              source={icons.pin}
+              style={{width: 25, height: 25, tintColor: COLORS.white}}
+            />
+          </View>
+        </View>
+      </Marker>
+    );
     return (
       <View style={{flex: 1}}>
         <MapView
           style={{flex: 1}}
           provider={PROVIDER_GOOGLE}
-          initialRegion={region}></MapView>
+          initialRegion={region}>
+          {destinationMarker()}
+        </MapView>
       </View>
     );
   };
