ff5cf03a0fc2880b1c355aa54f6e97730c67b2a3
Abdelhameed Mohamed <ben10a2992@gmail.com>
2021-03-12 22:59:26 +0200

    Add all files of react-native-maps

--- screens/OrderDelivary.js
+++ screens/OrderDelivary.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import MapView from 'react-native-maps';
 
 const OrederDelivary = () => {
   const renderMap = () => {
-    return <View></View>;
+    return (
+      <View style={{flex: 1}}>
+        <MapView style={{flex: 1}}></MapView>
+      </View>
+    );
   };
 
   return <View style={{flex: 1}}>{renderMap()}</View>;
